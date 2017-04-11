8e9ef2d1695990f5ceb7012e9f5df8e0a6a340f3
zhangmiaojie <136218949@qq.com>
2017-04-11 14:17:18 +0800

    elasticsearch实现文章的高亮搜索

--- mw-es-service/src/main/java/com/water/es/utils/ElasticSearchTemplate.java
+++ mw-es-service/src/main/java/com/water/es/utils/ElasticSearchTemplate.java
@@ -1,11 +1,6 @@
 package com.water.es.utils;
 
 import com.alibaba.fastjson.JSON;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import com.water.es.annotation.EsMapping;
 import com.water.es.entry.ITArticle;
 import org.apache.commons.logging.Log;
@@ -13,28 +8,28 @@ import org.apache.commons.logging.LogFactory;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.TermQueryBuilder;
-import org.elasticsearch.index.reindex.ScrollableHitSource;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.Type;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -55,6 +50,18 @@ public class ElasticSearchTemplate {
         } catch (Exception e) {
             logger.error(e);
         }
+    }
+
+    public Map<String, Object> getSetting() {
+        Map<String, Object> setting = new HashMap<String, Object>();
+        setting.put("index.number_of_shards", 5);// 创建后不能再修改
+        setting.put("index.refresh_interval", "-1");
+        setting.put("index.max_result_window", 500000);
+        setting.put("index.number_of_replicas", 0);// 创建时副本暂时为0，加快数据导入速度
+        // 在ES中，进行一次提交并删除事务日志的操作叫做 flush，一旦translog达到这个尺寸，刷新将会发生。默认为512 mb。
+        setting.put("index.translog.flush_threshold_size", "2GB");
+        setting.put("persistent.indices.store.throttle.max_bytes_per_sec", "150mb");// 对存储进行限流
+        return setting;
     }
 
     /**
@@ -78,6 +85,43 @@ public class ElasticSearchTemplate {
     }
 
     /**
+     * 根据对象创建索引文档
+     *
+     * @param cls
+     * @return
+     */
+    public XContentBuilder createIKMapping(Class cls) {
+        XContentBuilder xContentBuilder = null;
+        try {
+            xContentBuilder = XContentFactory.jsonBuilder();
+            xContentBuilder.startObject().startObject("properties");
+            for (Field field : cls.getDeclaredFields()) {
+                EsMapping annotation = field.getAnnotation(EsMapping.class);
+                if (annotation.isMapping()) {
+                    xContentBuilder = xContentBuilder.startObject(field.getName()).field("type", field.getType().getSimpleName().toLowerCase());
+                    if (annotation.isAnalyzer()) { //判断是否索引
+                        xContentBuilder = xContentBuilder.field("analyzer", annotation.indexAnalyzer()).field("search_analyzer", annotation.searchAnalyzer());
+                    } else {
+                        xContentBuilder = xContentBuilder.field("index", "not_analyzed");
+                    }
+
+                    if (annotation.isStore()) { //判断是否存储
+                        xContentBuilder = xContentBuilder.field("store", "yes");
+                    } else {
+                        xContentBuilder = xContentBuilder.field("store", "no");
+                    }
+                    xContentBuilder.endObject();
+                }
+            }
+            xContentBuilder.endObject().endObject();
+            System.out.println(xContentBuilder.string());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return xContentBuilder;
+    }
+
+    /**
      * 添加文档
      *
      * @param objStr 对象
@@ -92,16 +136,21 @@ public class ElasticSearchTemplate {
     }
 
     /**
-     * 获取文档
+     * 根据id获取文档
      *
      * @param index 索引
      * @param type  类型
      * @param id    主键
      * @return Map
      */
-    public Map<String, Object> getDocument(String index, String type, String id) {
+    public String getDocumentById(String index, String type, String id) {
         GetResponse getResponse = client.prepareGet(index, type, id).get();
-        return getResponse.getSource();
+        String results = "";
+        Map<String, Object> source = getResponse.getSource();
+        if (source != null) {
+            results = JSON.toJSONString(results);
+        }
+        return results;
     }
 
     /**
@@ -139,38 +188,14 @@ public class ElasticSearchTemplate {
         return null;
     }
 
-    public XContentBuilder createIKMapping(Class cls) {
-        XContentBuilder xContentBuilder = null;
-        try {
-            xContentBuilder = XContentFactory.jsonBuilder();
-            xContentBuilder.startObject().startObject("properties");
-            for (Field field : cls.getDeclaredFields()) {
-                EsMapping annotation = field.getAnnotation(EsMapping.class);
-                if (annotation.isMapping()) {
-                    xContentBuilder = xContentBuilder.startObject(field.getName()).field("type", field.getType().getSimpleName().toLowerCase());
-                    if (annotation.isAnalyzer()) { //判断是否索引
-                        xContentBuilder = xContentBuilder.field("analyzer", annotation.indexAnalyzer()).field("search_analyzer", annotation.searchAnalyzer());
-                    } else {
-                        xContentBuilder = xContentBuilder.field("index", "not_analyzed");
-                    }
 
-                    if (annotation.isStore()) { //判断是否存储
-                        xContentBuilder = xContentBuilder.field("store", "yes");
-                    } else {
-                        xContentBuilder = xContentBuilder.field("store", "no");
-                    }
-                    xContentBuilder.endObject();
-                }
-            }
-            xContentBuilder.endObject().endObject();
-            System.out.println(xContentBuilder.string());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return xContentBuilder;
-    }
-
-    public String matchQueryBuilder(String index, String type, String key, String value,int from, int size) {
+    /**
+     * 搜索文档 （分词搜索）
+     * 如果查询 日期（date） 或 整数（integer） 字段，它们会将查询字符串分别作为日期或整数对待。
+     * 如果查询一个（ not_analyzed ）未分析的精确值字符串字段， 它们会将整个查询字符串作为单个词项对待。
+     * 但如果要查询一个（ analyzed ）已分析的全文字段， 它们会先将查询字符串传递到一个合适的分析器，然后生成一个供查询的词项列表。
+     */
+    public String matchQueryBuilder(String index, String type, String key, String value, int from, int size) {
         MatchQueryBuilder queryBuilder = QueryBuilders.matchQuery(key, value);
         SearchResponse searchResponse = client.prepareSearch(index)
                 .setTypes(type)
@@ -193,9 +218,8 @@ public class ElasticSearchTemplate {
     }
 
     /**
-     * 搜索文档
-     *
-     * @param queryValue
+     * 搜索文档 (精确搜索)
+     * 它不会对词的多样性进行处理（如， foo 或 FOO ）
      */
     public String searchDocumentByTerm(String index, String type, String queryField, String queryValue, int from, int size) {
         QueryBuilder queryBuilder = new TermQueryBuilder(queryField, queryValue);
@@ -219,15 +243,100 @@ public class ElasticSearchTemplate {
         return JSON.toJSONString(results);
     }
 
+    public String searchDocumentShouldMatch(String index, String type, String queryField, String queryValue, int from, int size) {
+        QueryBuilder queryBuilder = QueryBuilders.boolQuery()
+                .should(new TermQueryBuilder(queryField, queryValue))
+                .should(new TermQueryBuilder(queryField, queryValue));
+
+        SearchResponse searchResponse = client.prepareSearch(index)
+                .setTypes(type)
+                .setQuery(queryBuilder)
+                .setFrom(from)
+                .setSize(size)
+                .execute()
+                .actionGet();
+        SearchHits searchHit = searchResponse.getHits();
+        Map<String, Object> sources;
+        List<Map<String, Object>> results = new ArrayList<Map<String, Object>>();
+        if (searchHit.totalHits() > 0) {
+            for (SearchHit hit : searchHit.getHits()) {
+                sources = hit.getSource();
+                results.add(sources);
+            }
+        }
+        return JSON.toJSONString(results);
+    }
+
+    public List<ITArticle> searchArticleByHighLight(String key) {
+        List<ITArticle> articleList = new ArrayList<ITArticle>();
+        try {
+
+            HighlightBuilder highlightBuilder = new HighlightBuilder();
+            highlightBuilder.field("title");
+            highlightBuilder.field("content");
+            highlightBuilder.preTags("<strong style='color:red;'>");
+            highlightBuilder.postTags("</strong>");
+            SearchRequestBuilder searchRequestBuilder = client.prepareSearch("blog")
+                    .highlighter(highlightBuilder);
+            searchRequestBuilder.setTypes("article");
+            searchRequestBuilder.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+
+            QueryStringQueryBuilder queryBuilder = new QueryStringQueryBuilder(key);
+            queryBuilder.analyzer("ik_smart");
+            queryBuilder.field("title").field("content");
+            searchRequestBuilder.setQuery(queryBuilder);
+            searchRequestBuilder.setFrom(0).setSize(20);
+            SearchResponse response = searchRequestBuilder.execute()
+                    .actionGet();
+
+            // 获取搜索的文档结果
+            SearchHits searchHits = response.getHits();
+            SearchHit[] hits = searchHits.getHits();
+            for (int i = 0; i < hits.length; i++) {
+                SearchHit hit = hits[i];
+                // 将文档中的每一个对象转换json串值
+                String json = hit.getSourceAsString();
+//                System.out.println(json);
+
+                Map<String, HighlightField> result = hit.highlightFields();
+                //从设定的高亮域中取得指定域
+                HighlightField titleField = result.get("title");
+                //取得定义的高亮标签
+                if (titleField != null) {
+                    Text[] titleTexts =  titleField.fragments();
+                    //为title串值增加自定义的高亮标签
+                    String title = "";
+                    for(Text text : titleTexts){
+                        title += text;
+                    }
+                    System.out.println(title);
+                }
+
+
+                HighlightField contentField = result.get("content");
+                //取得定义的高亮标签
+                if (contentField != null) {
+                    Text[] contentTexts =  contentField.fragments();
+                    //为title串值增加自定义的高亮标签
+                    String content = "";
+                    for(Text text : contentTexts){
+                        content += text;
+                    }
+                    System.out.println(content);
+                }
+                System.out.println("__________________________________________________________________________________");
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        ElasticSearchTemplate elasticSearchTemplate = new ElasticSearchTemplate();
-//        String result = elasticSearchTemplate.matchQueryBuilder("blog", "article", "content", "密码列表");
-        String result = elasticSearchTemplate.searchDocumentByTerm("blog", "article", "content", "密码列表", 0, 20);
-        System.out.println(result);
-        Gson gson = new Gson();
-        List<ITArticle> itArticles;
-        Type type = new TypeToken<ArrayList<ITArticle>>() {
-        }.getType();
-        itArticles = gson.fromJson(result, type);
+        ElasticSearchTemplate util = new ElasticSearchTemplate();
+        util.searchArticleByHighLight("java");
+
+//        util.createIndex("blog","article",ITArticle.class);
     }
 }
